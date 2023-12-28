fce70d410e17298ff81dba621d2068fa045e14e6
Yaswanth kumar <yaswa779@gmail.com>
2023-12-28 09:56:55 +0530

    test route added

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/test')
+def test():
+    return "test route"
+
 @app.route('/')
 def home():
     return 'Hello, Every One Welcome to Azure Web App with Flask!'
