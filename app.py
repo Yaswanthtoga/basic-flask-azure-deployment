8006b7f8bdb1e726f99bedf91050f3f9da15146c
Yaswanth kumar <yaswa779@gmail.com>
2023-12-28 09:53:31 +0530

    changed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, Azure Web App with Flask!'
+    return 'Hello, Every One Welcome to Azure Web App with Flask!'
 
 if __name__ == '__main__':
     app.run()
